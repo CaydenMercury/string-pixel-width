bbab25b1310e9cf337f5f91931f9bacd31c8b2f6
Cayden D <140987846+CaydenMercury@users.noreply.github.com>
2025-11-20 12:50:39 +1100

    Version update, added widthsMapV2 (stronger pixel calculations)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import deburr from 'lodash.deburr';
 import widthsMap from './widthsMap';
+import widthsMapV2 from './widthsMapV2';
 
-const settingsDefaults = { font: 'Arial', size: 100 };
+const settingsDefaults = { font: 'Arial', size: 100, fontWeight: 400, version: 1 };
+const availableFontWeights = [400, 500, 600, 700];
 
 const getWidth = (string, settings) => {
   const sett = { ...settingsDefaults, ...settings };
+
   const font = sett.font.toLowerCase();
   const size = sett.size;
-  const variant = 0 + (sett.bold ? 1 : 0) + (sett.italic ? 2 : 0);
-  const map = sett.map || widthsMap;
+  const version = typeof sett.version == 'undefined' ? 1 : sett.version;
+  const variant = typeof sett.fontWeight != 'undefined' && availableFontWeights.indexOf(sett.fontWeight) > -1 ? availableFontWeights.indexOf(sett.fontWeight) : 0;
+
+  const map = version > 1 ? widthsMapV2 : widthsMap;
   const available = Object.keys(map);
   if (available.indexOf(font) === -1) {
     throw new Error(`This font is not supported. Supported fonts are: ${available.join(', ')}`);
   }
+  
   let totalWidth = 0;
   deburr(string).split('').forEach((char) => {
     if (/[\x00-\x1F]/.test(char)) { // non-printable character
